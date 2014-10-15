3de815736c7743f23f83d12a16fefe46496bff79
robksawyer <robksawyer@gmail.com>
2014-10-15 12:30:49 -0700

    Changed up the API strategy a bit

--- api/controllers/ArtistController.js
+++ api/controllers/ArtistController.js
@@ -8,16 +8,22 @@
 module.exports = {
 	
 	score: function(req, res) {
-		
-		Klout.klout.getKloutIdentity(req.param(0), "tw", function(err, k_res){
-			if(err) return res.json(err);
+		if(typeof(req.param('score')) !== undefined ){
 
-			if(k_res && k_res.id){
-				
-				return res.redirect('/artist/score_response?klout_id=' + k_res.id);
+			Klout.init(); //Initialize the API
+			Klout.klout.getKloutIdentity(req.param('score'), "tw", function(err, k_res){
+				if(err) return res.json(err);
 
-			}
-		});
+				if(k_res && k_res.id){
+					
+					return res.redirect('/artist/score_response?klout_id=' + k_res.id);
+
+				}
+			});
+
+		} else {
+			return res.json({'error':'score= is the only property allowed at this time.'});
+		}
 	},
 
 	score_response: function(req, res){
